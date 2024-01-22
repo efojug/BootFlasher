7b90a74aae2be15be99976cf792204dc0d221c3a
efojug <lidudu890890@163.com>
2024-01-22 18:36:28 +0800

    fix bug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.efojug.bootflasher"
         minSdk = 30
         targetSdk = 34
-        versionCode = 60
-        versionName = "2.1"
+        versionCode = 64
+        versionName = "2.2"
     }
 
     buildTypes {
