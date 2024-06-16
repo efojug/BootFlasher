728bfd22695f2f3b8ebc194adee035d5d5e90835
Lucius <dmdxcz@gmail.com>
2024-06-16 22:25:11 +0800

    change kotlin version to 2.0, add kotlinx-coroutines dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.androidApplication) apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0-Beta2" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
 }
